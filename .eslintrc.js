b3355ab029b597d788dd58f71bfc46f0a38dcb20
nam-hackintosh <lenkc98@gmail.com>
2019-03-30 12:15:44 +0700

    Resovle eslint, babel

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'extends': 'airbnb',
+  'parser': 'babel-eslint',
+  'env': {
+    'jest': true,
+  },
+  'rules': {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'import/no-unresolved': 'off',
+  },
+  'globals': {
+    "fetch": false
+  }
+}
